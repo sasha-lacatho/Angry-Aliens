26603c002aba945e7ea4eb293283b6caba3be5b1
P3D\thomas.mol <tm.adm1718@gmail.com>
2023-09-14 17:08:36 +0200

    amélioration du code de tour par tour (pas tout à fait fini)

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -2,66 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class TurnManager: MonoBehaviour
+public class TurnManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI _textTeam;//display name of the team
     [SerializeField]
-    private TextMeshProUGUI _textTimer1;//display timer for the team 1
-    [SerializeField]
-    private TextMeshProUGUI _textTimer2;//display timer for the team 2
+    private TextMeshProUGUI[] _textTimers;//display timer for the teams
+    //[SerializeField]
+    //private TextMeshProUGUI _textTimer2;//display timer for the team 2
     [SerializeField]
     private TextMeshProUGUI _textTeamChange;//displayed when changing team
-    private int _team = 1;
-    private float _now;//time at the start of the turn
-    private int _duration = 15;// duration of a turn
-    private int _timeOnScreen = 3;//time On Screen for text in the middle of the screen
+
+    [SerializeField] private int _teamId = 0;
+    [SerializeField] private int _characterId = 0;
+    private int _now;//time at the start of the turn
+
+    public int timerDuration = 15;// duration of a turn
+    public int popupDuration = 3;//time On Screen for text in the middle of the screen
+    /*
+    public List<int> characterTeam1 = new List<int>();
+    public List<int> characterTeam2 = new List<int>();
+    */
+    public LinkedList<int> characterTeam1 = new LinkedList<int>();
+    public LinkedList<int> characterTeam2 = new LinkedList<int>();
+
     // Start is called before the first frame update
     void Start()
     {
         _now = (int)Time.time;
-        _textTeam.text = $"TEAM {_team}";
-        _textTeamChange.text = $"TEAM {_team} TURN!";
+        ShowTeamNumber();
+
     }
 
+    private void ShowTeamNumber()
+    {
+        _textTeam.text = $"TEAM {_teamId + 1}";
+        _textTeamChange.text = $"TEAM {_teamId + 1} TURN!";
+    }
+
+    private void ShowTeamTimer()
+    {
+        _textTimers[_teamId].text = $"0:{_now + timerDuration - (int)Time.time}";
+        _textTimers[(_teamId+1)%2].text = "";
+    }
     // Update is called once per frame
     void Update()
     {
-
-        if (_team == 1)//popup
+        ShowTeamTimer();
+        //if (_teamId == 1)//popup
+        //{
+        //    _textTimer1.text = $"0:{_now + timer_duration - (int)Time.time}";
+        //    _textTimer2.text = "";
+        //    if (_now + timeOnScreen - (int)Time.time == 0 && _textTeamChange.text != "")
+        //    {
+        //        _textTeamChange.text = "";
+        //    }
+        //}
+        //else
+        //{
+        //    _textTimer1.text = "";
+        //    _textTimer2.text = $"0:{_now + timer_duration - (int)Time.time}";
+        //    if (_now + timeOnScreen - (int)Time.time == 0)
+        //    {
+        //        _textTeamChange.text = "";
+        //    }
+        //}
+        //_timer -= Time.deltaTime;
+        //if (_timer <= 0)
+        //{
+        //    _timer = timer_duration;
+        //}
+        if (_now < Time.time - timerDuration)
         {
-            _textTimer1.text = $"0:{_now + _duration - (int)Time.time}";
-            _textTimer2.text = "";
-            if (_now + _timeOnScreen - (int)Time.time == 0 && _textTeamChange.text != "")
+            _teamId = (_teamId + 1) % 2; // on passe de 0 à 1 puis de 1 à 0
+            if (_teamId == 0)
             {
-                _textTeamChange.text = "";
+                _characterId = (_characterId + 1) % 4;// on passe de 0 à 1 puis 2 puis 3 puis 0 etc.
             }
-        }
-        else
-        {
-            _textTimer1.text = "";
-            _textTimer2.text = $"0:{_now + _duration - (int)Time.time}";
-            if (_now + _timeOnScreen - (int)Time.time == 0)
-            {
-                _textTeamChange.text = "";
-            }
-        }
-
-        if (_now < Time.time - _duration)
-        {
-            if (_team == 1) //switch team
-            {
-                _team = 2;
-                _textTeam.text = $"TEAM {_team}";
-                _textTeamChange.text = $"TEAM {_team} TURN!";
-            }
-            else
-            {
-                _team = 1;
-                _textTeam.text = $"TEAM {_team}";
-                _textTeamChange.text = $"TEAM {_team} TURN!";
-            }
-
+            ShowTeamNumber();
             _now = (int)Time.time;//restart timer
         }
     }
